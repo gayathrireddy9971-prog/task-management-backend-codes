f9e7e0770ec42f176d33a327a4e40a99fc0e3972
Gayatri Reddy <gayathrireddy9971@gmail.com>
2026-05-15 08:41:18 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const allRoutes = require("./src/routes");
+app.use("/api", allRoutes);
+
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Backend Running 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
