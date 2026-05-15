8b0d7c55eccc036eeceea57870ad978aa3a91c96
Gayatri Reddy <gayathrireddy9971@gmail.com>
2026-05-15 08:49:32 +0530

    Create auth.controller.js

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,43 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+const generateToken = require("../utils/generateToken");
+
+exports.register = async (req, res) => {
+  try {
+    let { name, email, password } = req.body;
+
+    name = name?.trim();
+    email = email?.toLowerCase().trim();
+
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role: "member",
+    });
+
+    return res.status(201).json({
+      token: generateToken(user._id),
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+  } catch (err) {
+    console.error("REGISTER ERROR:", err);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
