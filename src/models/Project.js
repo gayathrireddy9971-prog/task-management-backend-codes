98ce3a2e4486ce76aa75ef84a26bb20f8e0d416f
Gayatri Reddy <gayathrireddy9971@gmail.com>
2026-05-15 08:56:01 +0530

    Create Project.js

---
+++ src/models/Project.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const projectSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: String,
+
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+
+    tasks: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Project", projectSchema);
